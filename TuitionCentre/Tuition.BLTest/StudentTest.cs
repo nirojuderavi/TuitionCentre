57dff02287ff43b56d054b37b6a5634d5799ba58
nirojuderavi <niroshan.ravindrakumar@umusic.com>
2017-05-25 17:55:34 +0100

    Uploading solution
    Uploading solution

---
+++ TuitionCentre/Tuition.BLTest/StudentTest.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TuitionCentre.BL;
+
+namespace Tuition.BLTest
+{
+    [TestClass]
+    public class StudentTest
+    {
+        [TestMethod]
+        public void FullNameTestValidation()
+        {
+
+            //Arrange
+            Student student = new Student();
+            student.FirstName = "Niro";
+            student.LastName = "Ravi";
+            string expected = "Niro Ravi";
+            //Act
+
+            string actual = student.FullName;
+
+            //Assert
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestMethod]
+        public void FullNameFirstNameEmpty()
+        {
+
+            //Arrange
+            Student student = new Student();
+            
+            student.LastName = "Ravi";
+            string expected = "Ravi";
+            //Act
+
+            string actual = student.FullName;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FullNameLastNameEmpty()
+        {
+
+            //Arrange
+            Student student = new Student();
+            student.FirstName = "Niro";
+            
+            string expected = "Niro";
+            //Act
+
+            string actual = student.FullName;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void InstanceCount()
+        {
+
+            //Arrange
+            Student student1 = new Student();
+            student1.FirstName = "Niro";
+            Student.InstanceCount++;
+
+            Student student2 = new Student();
+            student2.FirstName = "Anthony";
+            Student.InstanceCount++;
+
+            Student student3 = new Student();
+            student3.FirstName = "Nili";
+            Student.InstanceCount++;
+            //Act
+
+            int actual = Student.InstanceCount;
+            int expected = 3;
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ValidateValid()
+        {
+
+            //Arrange
+
+            var student = new Student();
+            student.LastName = "Ravi";
+            student.StudentEmail = "niroravio@example.com";
+            var expected = true;
+
+            //Act
+
+            var actual = student.Validate();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ValidateInValid()
+        {
+
+            //Arrange
+
+            var student = new Student();
+            student.LastName = "Ravi";
+            var expected = false;
+
+            //Act
+
+            var actual = student.Validate();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ TuitionCentre/TuitionCentre.BL/Student.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuitionCentre.BL
+{
+    public class Student
+    {
+
+        public Student()
+        {
+                
+        }
+
+        public Student(int studnetId)
+        {
+            this.StudentId = studnetId;
+        }
+
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string StudentEmail { get; set; }
+
+        public int StudentId { get; private set; }
+
+        public static int InstanceCount { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                string fullName = FirstName;
+                if (!string.IsNullOrWhiteSpace(LastName))
+                {
+                    if (!string.IsNullOrWhiteSpace(fullName))
+                    {
+                        fullName += " ";
+                    }
+                    fullName += LastName;
+                }
+                return fullName;
+            }
+        }
+
+        public Student Retrieve(int studentId)
+        {
+            return new Student();
+        }
+
+        public List<Student> Retrieve()
+        {
+            return new List<Student>();
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+
+
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (string.IsNullOrWhiteSpace(LastName))
+            {
+                isValid = false;
+            }
+
+            if (string.IsNullOrWhiteSpace(StudentEmail))
+            {
+                isValid = false;
+            }
+
+            return isValid;
+        }
+    }
+}
